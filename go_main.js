7810a43bd4c885e3470d35b7c3afeed01dd3c1c9
Nnavarro9 <100961745+Nnavarro9@users.noreply.github.com>
2023-12-12 15:51:39 +0100

    Delete go_main.js

--- go_main.js
+++
@@ -1,8 +0,0 @@
-const regsitrarButton = document.getElementById("logejat");
-//const correoField = document.getElementById("correo");
-//const passwordField = document.getElementById("contrasenya");
-// Agregar un controlador de events al botó
-registrarButton.addEventListener("click", function() {
-    // Redirigir a la página "registre.html"
-    window.location.href = "mainprova.html?";
-});
